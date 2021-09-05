ec78bef9c96316ea801bf51f5bf2bd6ffae83080
Jant <63055012+HamnYce@users.noreply.github.com>
2021-09-05 22:25:02 +0300

    Update program.cs

--- Temperature/Program.cs
+++ Temperature/Program.cs
@@ -7,17 +7,14 @@ namespace Temperature
         static void Main(string[] args)
         {
             int temperature = 0;
-            Console.WriteLine("pleasrue enter temperature :)");
+            Console.WriteLine("please enter temperature :)");
 
             //Converting string to integer, from user input (ReadLine)
             temperature = Convert.ToInt16(Console.ReadLine());
 
             
-            if (temperature <= 0)
-            {
-                Console.WriteLine("Water will freeze; the freezing point of water is 0°C.");
-            }
-            else if (temperature >= 208)
+           
+            if (temperature >= 208)
             {
                 Console.WriteLine("A good temperature for brewing black tea.");
             }
@@ -29,10 +26,13 @@ namespace Temperature
             {
                 Console.WriteLine("A good temperature for brewing white and green teas.");
             }
+            else  if (temperature <= 0)
+            {
+                Console.WriteLine("Water will freeze; the freezing point of water is 0°C.");
+            }
             else
             {
-                Console.WriteLine("Water is not cold enough to freeze,");
-                Console.WriteLine("nor a good temperature for brewing tea.");
+                Console.WriteLine("Water is not cold enough to freeze,\nnor a good temperature for brewing tea.");
             }
 
             Console.ReadKey();
